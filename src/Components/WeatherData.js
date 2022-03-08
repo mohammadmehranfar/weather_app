bba63b689b5c14f7f36bcf57845e48456276ff7d
Arsalan Mehranfar <mohammadmehranfar1999@gmail.com>
2022-03-09 02:26:26 +0330

    Left-Side

---
+++ src/Components/WeatherData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { fetchWeather } from '../api/fetchWeather';
+const WeatherData = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WeatherData;
